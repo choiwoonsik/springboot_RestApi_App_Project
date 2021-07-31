f75af5f57ab60328e97c75c2a5bcdd717ca1bfdb
woonsik <dnstlr2933@naver.com>
2021-07-31 22:21:43 +0900

    first commit

---
+++ restApiSpringBootApp/src/main/java/com/restApi/restApiSpringBootApp/RestApiSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.restApi.restApiSpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiSpringBootAppApplication.class, args);
+	}
+
+}
---
+++ restApiSpringBootApp/src/test/java/com/restApi/restApiSpringBootApp/RestApiSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restApi.restApiSpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
